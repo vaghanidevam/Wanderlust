45d160dc27eae09ee2080b7851393efe462cb857
devam vaghani <vaghanidevam11@gmail.com>
2024-10-22 02:13:30 +0530

    new

--- app.js
+++ app.js
@@ -60,9 +60,9 @@ const store = MongoStore.create({
     },
     touchAfter: 24*3600
   });
-  store.on("error", ()=>{
-    console.log("error in mongo session store ", err);
-  });
+//   store.on("error", ()=>{
+//     console.log("error in mongo session store ", err);
+//   });
 
 const sessionOptions = {
     store:store,
